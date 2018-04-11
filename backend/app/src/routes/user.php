11987736c19f9cccd97e75bb7ad8ec92dff8f842
Daniel Kudernatsch <d.kudernatsch@outlook.com>
2018-04-11 18:42:02 +0200

    refactored user routes to separate file and updated manual tests

--- backend/app/src/routes/user.php
+++ backend/app/src/routes/user.php
@@ -4,14 +4,6 @@ use PDOs\User\UserDAO;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-// Routes
-
-$app->get('/hello[/[{name}]]', function (Request $request, Response $response, array $args) {
-    $this->logger->info("Hello World 'hello' route");
-    $hello["world"] = "hello " . $args['name'];
-    return $response->withJson($hello);
-});
-
 $app->get('/user/', function (Request $request, Response $response, array $args) {
     $this->logger->info("GET: all users");
     $repo = new UserDAO($this->db);
@@ -20,14 +12,13 @@ $app->get('/user/', function (Request $request, Response $response, array $args)
         return $response
             ->withStatus(200)
             ->withJson(["success" => ["users" => $user]]);
-    }else{
-        return $response
-            ->withStatus(404)
-            ->withJson(["error" => "User not found"]);
+    } else {
+        throw new \errors\HttpServerException(404, "Users not found");
     }
-});
+})->add(new ScopedJWTAuth(["admin"]));
 
 $app->get('/user/{id}', function (Request $request, Response $response, array $args) {
+
     $this->logger->info("GET: user with id " . $args['id']);
     $repo = new UserDAO($this->db);
     $user = $repo->byId($args['id']);
@@ -36,11 +27,9 @@ $app->get('/user/{id}', function (Request $request, Response $response, array $a
             ->withStatus(200)
             ->withJson(["success" => ["user" => $user]]);
     }else{
-        return $response
-            ->withStatus(404)
-            ->withJson(["error" => "User not found"]);
+        throw new \errors\HttpServerException(404, "Users not found");
     }
-});
+})->add(new ScopedJWTAuth(["user"]));
 
 
 $app->post("/user", function (Request $request, Response $response, array $args) {
@@ -48,15 +37,14 @@ $app->post("/user", function (Request $request, Response $response, array $args)
     $body = $request->getParsedBody();
 
     if(!isset($body['user'])) {
-        return $response
-            ->withStatus(400)
-            ->withJson(["error" => "Malformed request: Missing field user"]);
+        throw new \errors\HttpServerException(400, "Malformed request: Missing field user");
     }
-    $newUser = \PDOs\JsonMapper::map("\PDOs\NewUser", $body['user']);
-    $repo = new \PDOs\UserDAO($this->db);
+
+    $newUser = \PDOs\JsonMapper::map("\PDOs\User\NewUser", $body['user']);
+    $repo = new \PDOs\User\UserDAO($this->db);
     $userId = $repo->insertNew($newUser);
 
     return $response
         ->withStatus(200)
         ->withJson(["success" => ["id" => $userId]]);
-});
+})->add(new ScopedJWTAuth(["anonymous"]));
