821196fe7b89facf31c8160aa3ecc8ae5a260132
Viktor <viktor.leher@gmail.com>
2018-05-30 23:51:54 +0200

    added basic sub-navigation

--- frontend/webshop-app/src/app/app.module.ts
+++ frontend/webshop-app/src/app/app.module.ts
@@ -12,20 +12,33 @@ import { AppComponent } from './app.component';
 // for bootstrap
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
+// comps for header
 import {NavbarComponent} from './components/header/navbar/navbar.component';
 import {LoginComponent} from './components/header/login/login.component';
+
 import {HomePageComponent} from './components/home-page/home-page';
-import {AdminPageComponent} from './components/admin-page/admin-page';
+
 import {ProductPageComponent} from './components/product-page/product-page';
+
+import {AdminPageComponent} from './components/admin-page/admin-page';
+import {ManageCouponsPageComponent} from "./components/admin-page/manage-coupons-page/manage-coupons-page";
+import {ManageProductsPageComponent} from "./components/admin-page/manage-products-page/manage-products-page";
+import {ManageUsersPageComponent} from "./components/admin-page/manage-users-page/manage-users-page";
+
 import {ShoppingCartPageComponent} from './components/shoppingCart-page/shoppingCart-page';
+
 import {UserPageComponent} from './components/user-page/user-page';
 
+// set up the routes
 const appRoutes: Routes = [
   {path: '', component: HomePageComponent},
   {path: 'products', component: ProductPageComponent},
   {path: 'cart', component: ShoppingCartPageComponent},
   {path: 'account', component: UserPageComponent},
   {path: 'admin', component: AdminPageComponent},
+  {path: 'admin/products', component: ManageProductsPageComponent},
+  {path: 'admin/coupons', component: ManageCouponsPageComponent},
+  {path: 'admin/users', component: ManageUsersPageComponent},
 ];
 
 
@@ -35,6 +48,9 @@ const appRoutes: Routes = [
     NavbarComponent,
     HomePageComponent,
     AdminPageComponent,
+    ManageCouponsPageComponent,
+    ManageProductsPageComponent,
+    ManageUsersPageComponent,
     ProductPageComponent,
     ShoppingCartPageComponent,
     UserPageComponent,
---
+++ frontend/webshop-app/src/app/components/admin-page/manage-products-page/manage-products-page.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-manage-products-page',
+  templateUrl: './manage-products-page-comp.html'
+})
+export class ManageProductsPageComponent {
+
+}
