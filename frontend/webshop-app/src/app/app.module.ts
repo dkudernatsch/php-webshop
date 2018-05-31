e7d58144e36685f4cb2333307ae5e50cbc2f8685
Viktor <viktor.leher@gmail.com>
2018-05-31 17:18:22 +0200

    added default subroute

--- frontend/webshop-app/src/app/app.module.ts
+++ frontend/webshop-app/src/app/app.module.ts
@@ -21,13 +21,15 @@ import {HomePageComponent} from './components/home-page/home-page';
 import {ProductPageComponent} from './components/product-page/product-page';
 
 import {AdminPageComponent} from './components/admin-page/admin-page';
-import {ManageCouponsPageComponent} from "./components/admin-page/manage-coupons-page/manage-coupons-page";
-import {ManageProductsPageComponent} from "./components/admin-page/manage-products-page/manage-products-page";
-import {ManageUsersPageComponent} from "./components/admin-page/manage-users-page/manage-users-page";
+import {ManageCouponsPageComponent} from './components/admin-page/manage-coupons-page/manage-coupons-page';
+import {ManageProductsPageComponent} from './components/admin-page/manage-products-page/manage-products-page';
+import {ManageUsersPageComponent} from './components/admin-page/manage-users-page/manage-users-page';
 
 import {ShoppingCartPageComponent} from './components/shoppingCart-page/shoppingCart-page';
 
 import {UserPageComponent} from './components/user-page/user-page';
+
+import {PageNotFoundPageComponent} from './components/page-not-found/page-not-found-page';
 
 // set up the routes
 const appRoutes: Routes = [
@@ -40,8 +42,12 @@ const appRoutes: Routes = [
       {path: 'products', component: ManageProductsPageComponent},
       {path: 'coupons', component: ManageCouponsPageComponent},
       {path: 'users', component: ManageUsersPageComponent},
+      {path: '', redirectTo: 'products', pathMatch: 'full'},
     ]
-  }
+  },
+  {path: 'not-found', component: PageNotFoundPageComponent},
+  // has to be last entry!
+  {path: '**', redirectTo: '/not-found'}
 ];
 
 
@@ -56,6 +62,7 @@ const appRoutes: Routes = [
     ManageUsersPageComponent,
     ProductPageComponent,
     ShoppingCartPageComponent,
+    PageNotFoundPageComponent,
     UserPageComponent,
     LoginComponent
   ],
