f5dc050d096849b050dd7787e37119f43d46f369
Daniel Kudernatsch <d.kudernatsch@outlook.com>
2018-06-03 15:03:02 +0200

    improved auth services

---
+++ frontend/webshop-app/src/app/services/api/http-requestor.service.ts
@@ -0,0 +1,46 @@
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {flatMap, map} from 'rxjs/operators';
+import {ApiRequest, ApiResponse, FailedResponse, SuccessfulResponse, isFailedResponse, isSuccessResponse} from '../../types/api-request';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {AuthService} from '../auth/auth.service';
+import {Token} from '../auth/userAuth';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class HttpRequestorService {
+
+    private readonly apiUrl = 'https://api.webshop.at';
+
+
+
+    constructor(private http: HttpClient) {
+    }
+
+    public request<T>(request: ApiRequest<T>): Observable<T> {
+        return this.sendRequest(request).pipe(
+            map((response: ApiResponse<T>) => {
+                if (isFailedResponse(response)) {
+                    throw new Error('Request failed please try again');
+                }
+                if (isSuccessResponse(response)) {
+                    return response.success;
+                }
+            })
+        );
+    }
+
+    private sendRequest<T>(request: ApiRequest<T>): Observable<ApiResponse<T>> {
+        switch (request.method) {
+            case 'GET'    :
+                return this.http.get   <ApiResponse<T>>(this.apiUrl + '/' + request.resource, {});
+            case 'POST'   :
+                return this.http.post  <ApiResponse<T>>(this.apiUrl + '/' + request.resource, request.body);
+            case 'PUT'    :
+                return this.http.put   <ApiResponse<T>>(this.apiUrl + '/' + request.resource, request.body);
+            case 'DELETE' :
+                return this.http.delete<ApiResponse<T>>(this.apiUrl + '/' + request.resource, {});
+        }
+    }
+}
